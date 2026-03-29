58a640f31664c78947551a8317e272328a9a40f2
tjddms1010 <https://github.com/2026-FORZA/Cstudy_sungeunyun.git>
2026-03-29 18:16:33 +0900

    1_Lab01

---
+++ 1_Lab01_2.c
@@ -0,0 +1,19 @@
+// 10172
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+main(void)
+{
+	printf("|\\_/|\n");
+
+	printf("|q p|   /}\n");
+
+	printf("( 0 )");
+	printf("\"\"\"\\\n");
+
+	printf("|\"^\"`    |\n");
+
+	printf("||_/=\\\\__|");
+	return 0;
+
+}
