51c2efa91d1c3c4e6cbb9e8760fc4d0cebfa37b3
tjddms1010 <https://github.com/2026-FORZA/Cstudy_sungeunyun.git>
2026-05-04 22:50:19 +0900

    4_Lab04

---
+++ 4_Lab04_01.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		printf("%d ", i);
+	}
+
+	
+	return 0;
+}
