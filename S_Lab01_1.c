3d5dbc9082f5d67b3aa2ceb523d0ee20d354d3f9
tjddms1010 <https://github.com/2026-FORZA/Cstudy_sungeunyun.git>
2026-07-12 19:55:08 +0900

    S_Lab01

---
+++ S_Lab01_1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n, k;
+	int cnt = 0;
+
+	scanf("%d %d", &n, &k);
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0) {
+			cnt++;
+			if (cnt == k) {
+				printf("%d\n", i);
+				return 0;
+			}
+		}
+	}
+
+		printf("0\n");
+	return 0;
+}
